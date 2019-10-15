d153228dae9b0da0a76c9402e1cb7e147e7124d6
wangyanlongwb <wangyanlongwb@bbk.com>
2019-10-15 17:26:35 +0800

    add hangman game

--- hangman.py
+++ hangman.py
@@ -62,3 +62,57 @@ def displayBoard(missedLetters, correctLetters, secretWord):
     for letter in blanks:
         print(letter, end=" ")
     print()
+
+def getGuess(alreadyGuess):
+    while True:
+        print("Guess a letter.")
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print("Please enter a single letter.")
+        elif guess in alreadyGuess:
+            print('You have already guessed that letter, Choose again')
+        elif guess not in 'abcdefghigklmnopqrstuvwxyz':
+            print("Please enter a LETTER.")
+        else:
+            return guess
+
+def playAgain():
+    print('Do you want to play again? (yes or no)')
+    return input().lower().startswith('y')
+
+print("H A N G M A N")
+missedLetters = ""
+correctLetters = ""
+secretWord = getRandomWord(words)
+gameIsDone = False
+
+while True:
+    displayBoard(missedLetters, correctLetters, secretWord)
+    guess = getGuess(missedLetters + correctLetters)
+    if guess in secretWord:
+        correctLetters = correctLetters + guess
+        foundAllLetter = True
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetter = False
+                break
+        if foundAllLetter:
+            print("you win")
+            gameIsDone = True
+    else:
+        missedLetters = missedLetters + guess
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
+            displayBoard(missedLetters,correctLetters,secretWord)
+            print("You have run out of guess ! \n after" + str(len(missedLetters))+"missed guesses and"
+                  + str(len(correctLetters))+"correct guesses, the word was"+ secretWord+"\"")
+            gameIsDone = True
+
+    if gameIsDone:
+        if playAgain():
+            missedLetters = ""
+            correctLetters = ""
+            gameIsDone = False
+            secretWord = getRandomWord(words)
+        else:
+            break
